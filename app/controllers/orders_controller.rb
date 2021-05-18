569fc8b9a843d9f2d3e4f21c29a51a526fdbcf01
mohammedegila <mohammedegila@gmail.com>
2021-05-18 22:11:49 +0200

    Add welcome, navigation and footer views update orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,74 +1,66 @@
 class OrdersController < ApplicationController
     before_action :set_order, only: [:show, :edit, :update, :destroy]
-  
-    # GET /orders
-    # GET /orders.json
+    before_action :authenticate_user!, :except => [:show, :index]
+
     def index
-      @orders = Order.all
+        @orders = Order.all()
+       
     end
-  
-    # GET /orders/1
-    # GET /orders/1.json
-    def show
-    end
-  
-    # GET /orders/new
+
     def new
-      @order = Order.new
+        @order = Order.new
     end
-  
-    # GET /orders/1/edit
-    def edit
-    end
-  
-    # POST /orders
-    # POST /orders.json
+
     def create
-      @order = Order.new(order_params)
-  
-      respond_to do |format|
-        if @order.save
-          format.html { redirect_to @order, notice: 'Order was successfully created.' }
-          format.json { render :show, status: :created, location: @order }
-        else
-          format.html { render :new }
-          format.json { render json: @order.errors, status: :unprocessable_entity }
+        @order = Order.new(order_params)
+        respond_to do |format|
+          if @order.save
+            format.html { redirect_to @order, notice: 'Order was successfully created.' }
+            format.json { render :show, status: :created, location: @order }
+          else
+            format.html { render :new }
+            format.json { render json: @order.errors, status: :unprocessable_entity }
+          end
         end
       end
-    end
-  
-    # PATCH/PUT /orders/1
-    # PATCH/PUT /orders/1.json
-    def update
-      respond_to do |format|
-        if @order.update(order_params)
-          format.html { redirect_to @order, notice: 'Order was successfully updated.' }
-          format.json { render :show, status: :ok, location: @order }
-        else
-          format.html { render :edit }
-          format.json { render json: @order.errors, status: :unprocessable_entity }
-        end
-      end
-    end
-  
-    # DELETE /orders/1
-    # DELETE /orders/1.json
-    def destroy
-      @order.destroy
-      respond_to do |format|
-        format.html { redirect_to orders_url, notice: 'Order was successfully destroyed.' }
-        format.json { head :no_content }
-      end
-    end
-  
-    private
-      # Use callbacks to share common setup or constraints between actions.
-      def set_order
+
+    def show
         @order = Order.find(params[:id])
+    end
+
+    def edit
+        @order = Order.find(params[:id])
+    end
+
+    def update
+        respond_to do |format|
+          if @order.update(order_params)
+            format.html { redirect_to @order, notice: 'Order was successfully updated.' }
+            format.json { render :show, status: :ok, location: @order }
+          else
+            format.html { render :edit }
+            format.json { render json: @order.errors, status: :unprocessable_entity }
+          end
+        end
       end
-  
-      # Only allow a list of trusted parameters through.
-      def order_params
-        params.fetch(:order, {})
+
+      def destroy
+        @order.destroy
+        respond_to do |format|
+          format.html { redirect_to orders_url, notice: 'Order was successfully destroyed.' }
+          format.json { head :no_content }
+        end
       end
-  end
+
+      private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_order
+      @order = Order.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def order_params
+      params.fetch(:order, {})
+      params.fetch(:order, {}).permit(:search)
+    end
+end
