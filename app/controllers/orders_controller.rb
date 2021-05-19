452389c001b3375d55c2a312a77fe03c857c4b96
Ahmed <a85851a@gmail.com>
2021-05-19 06:57:36 +0200

    handle error styling

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,5 @@
 class OrdersController < ApplicationController
     before_action :set_order, only: [:show, :edit, :update, :destroy]
-    before_action :authenticate_user!, :except => [:show, :index]
 
     def index
         @orders = Order.all()
