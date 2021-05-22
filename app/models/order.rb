ea9f605d7bd6d95fb94fce8e4ef42652dc4073a4
Islam Shaaban <islamshaaban10@gmail.com>
2021-05-22 23:45:12 +0200

    Fix Some Bug

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,6 @@
 class Order < ApplicationRecord
     belongs_to :user
-    has_one :address
+    has_one :addresses
     has_many :order_products
     has_many :products, through: :order_products
     validates :state, acceptance: { accept: ['inCart', 'pending', 'confirmed', 'delivered'] }
